29bc4ec81e250239e5a74a3eb79486b357eeba90
pendekar-koding <pendekarkoding20@gmail.com>
2022-05-21 15:15:08 +0700

    first commit

---
+++ src/main/java/com/study/crudrestapi/common/exception/ErrorCode.java
@@ -0,0 +1,5 @@
+package com.study.crudrestapi.common.exception;
+
+public enum ErrorCode {
+    GENERIC_FAILURE
+}
---
+++ src/main/java/com/study/crudrestapi/common/response/CommonResponses.java
@@ -0,0 +1,37 @@
+package com.study.crudrestapi.common.response;
+
+
+import com.study.crudrestapi.common.controller.BaseController;
+
+public class CommonResponses<T> extends BaseController {
+
+	public CustomReturn<T> commonSuccessResponse(T wrapper) {
+		CustomReturn<T> customReturn = new CustomReturn<>();
+		customReturn.setMessage(STR_SUCCESS);
+		customReturn.setStatus("200");
+		customReturn.setDatas(wrapper);
+		return customReturn;
+	}
+
+	public CustomReturn<T> commonFailedResponse() {
+		CustomReturn<T> customReturn = new CustomReturn<>();
+		customReturn.setMessage(STR_FAILED);
+		customReturn.setStatus("200");
+		return customReturn;
+	}
+
+	public CustomReturn<T> commonFailedError() {
+		CustomReturn<T> customReturn = new CustomReturn<>();
+		customReturn.setMessage(STR_ERROR);
+		customReturn.setStatus("500");
+		return customReturn;
+	}
+
+	public CustomReturn<T> commonDeleteSuccess(){
+		CustomReturn<T> customReturn = new CustomReturn<>();
+		customReturn.setMessage(STR_SUCCESS);
+		customReturn.setStatus("200");
+		return customReturn;
+	}
+
+}
